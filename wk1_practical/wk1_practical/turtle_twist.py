7f0455a552ef288722138f523157fe99ba72719a
auro <auro@aurovm.myguest.virtualbox.org>
2024-10-08 14:51:57 +0100

    chore: patched turtle twist command to run as expected

--- wk1_practical/wk1_practical/turtle_twist.py
+++ wk1_practical/wk1_practical/turtle_twist.py
@@ -2,14 +2,15 @@ import rclpy
 from rclpy.node import Node
 
 from std_msgs.msg import String
+from geometry_msgs.msg import Twist, Vector3
 
 class TurtleTwist(Node):
 
 	def __init__(self):
 		super().__init__('turtle_twist')
-		self.publisher_ = self.create_publisher(
-			String,
-			'turtle1/cmd_vel',
+		self.publisher = self.create_publisher(
+			Twist,
+			'/turtle1/cmd_vel',
 			10)
 		timer_period = 0.5
 		self.timer = self.create_timer(timer_period, self.timer_callback)
@@ -18,10 +19,9 @@ class TurtleTwist(Node):
 	
 		
 	def timer_callback(self):
-		msg = String()
-		msg.data = '{linear: {x: 2.0, y: 0.0, z:0.0}, angular: {x: 0.0, y: 0.0, z:1.8}}'
-		self.publisher_.publish(msg)
-		self.get_logger().info('Publishing Turtle Twist Command: "%s"' % msg.data)
+		msg = Twist(linear = Vector3(x = 2.0, y = 0.0, z = 0.0), angular = Vector3(x = 0.0, y = 0.0, z = 1.8))
+		self.publisher.publish(msg)
+		self.get_logger().info('Publishing Turtle Twist Command')
 		self.i += 1 
 		
 def main(args=None):
