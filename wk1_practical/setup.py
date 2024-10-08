52c15cd9d6f1845bd41b194d7ec909c50f60cbef
Stephen Ingham <stephenmichael833@gmail.com>
2024-10-08 12:15:40 +0100

    chore: added and configured initial setup files for wk1_practical package

---
+++ wk1_practical/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+package_name = 'wk1_practical'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Stephen Ingham',
+    maintainer_email='stephenmichael833@gmail.com',
+    description='My package for wk1 practical',
+    license='Apache License 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        	'turtle_topic_echo = wk1_practical.topic_echo:main',
+        	'turtle_do_twist = wk1_practical.turtle_twist:main'
+        ],
+    },
+)
