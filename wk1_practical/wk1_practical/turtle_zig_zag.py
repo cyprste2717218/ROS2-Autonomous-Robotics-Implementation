313458d240422115e6daa9d83ee449d9c1099e9d
auro <auro@aurovm.myguest.virtualbox.org>
2024-10-09 15:59:40 +0100

    chore: configured zig zag path with spin at the end for turtle node

--- wk1_practical/wk1_practical/turtle_zig_zag.py
+++ wk1_practical/wk1_practical/turtle_zig_zag.py
@@ -12,34 +12,43 @@ class TurtleZigZag(Node):
 			Twist,
 			'/turtle1/cmd_vel',
 			10)
-		timer_period = 0.5
+		timer_period = 5
 		self.timer = self.create_timer(timer_period, self.timer_callback)
 		self.i = 0
 		
 	def zig(self):
 		#twist left and move forward
-
-		msg = Twist(linear = Vector3(x = 2.0, y = 10.0, z = 0.0), angular = Vector3(x = 0.0, y = 0.0, z = 10.0))
-		self.publisher.publish(msg)
+		msg_1 = Twist(linear = Vector3(x = 0.0, y = 0.0, z = 0.0), angular = Vector3(x = 0.0, y = 0.0, z = 90.0))
+		
+		msg_2 = Twist(linear = Vector3(x = 5.0, y = 5.0, z = 0.0), angular = Vector3(x = 0.0, y = 0.0, z = 0.0))
+		self.publisher.publish(msg_1)
+		self.publisher.publish(msg_2)
 		
 	def zag(self):
 		#twist right and move forward
-
-		msg = Twist(linear = Vector3(x = 2.0, y = 0.0, z = 0.0), angular = Vector3(x = 0.0, y = -1.8, z = 0.0))
-		self.publisher.publish(msg)
+		
+		msg_1 = Twist(linear = Vector3(x = 0.0, y = 0.0, z = 0.0), angular = Vector3(x = 0.0, y = 0.0, z = -90.0))
+		
+		msg_2 = Twist(linear = Vector3(x = 0.0, y = 5.0, z = 5.0), angular = Vector3(x = 0.0, y = 0.0, z = 0.0))
+		self.publisher.publish(msg_1)
+		self.publisher.publish(msg_2)
 		
 	def rotate_back(self):
 		#rotate 180 deg
 
-		msg = Twist(linear = Vector3(x = 0.0, y = 0.0, z = 0.0), angular = Vector3(x = 0.0, y = 180.0, z = 0.0))
+		msg = Twist(linear = Vector3(x = 0.0, y = 0.0, z = 0.0), angular = Vector3(x = 0.0, y = 0.0, z = 180.0))
 		self.publisher.publish(msg)
 		
 	def timer_callback(self):
 		
-		while self.i <= 5:
-			self.zig()
-			self.zag()
-			self.get_logger().info('Publishing Turtle Zig Zag Command')
+		while self.i <= 20000:
+		
+			if self.i % 2 != 0:
+				
+				self.zig()
+			else:
+				self.zag()
+		
 			self.i += 1
 		else:
 			self.rotate_back()
