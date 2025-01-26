e5e8c9199c5f2043431c4f39f5c64122a4f4e47d
auro <auro@aurovm.myguest.virtualbox.org>
2025-01-26 17:03:07 +0000

    chore: futher fleshing out of action client for held item colour action node

--- solution/resource/action_clients/current_held_colour_action_client.py
+++ solution/resource/action_clients/current_held_colour_action_client.py
@@ -9,13 +9,30 @@ class HeldColourActionClient(Node):
         super().__init__('held_colour_action_client')
         self._action_client = ActionClient(self, HeldItem, 'heldItem')
 
-    def send_goal(self, data):
+    def send_goal(self, item_colour):
         goal_msg = HeldItem.Goal()
-        goal_msg.data = data
+        goal_msg.item_colour = item_colour
 
         self._action_client.wait_for_server()
 
         return self._action_client.send_goal_async(goal_msg)
+    
+    def goal_response_callback(self, future):
+        goal_handle = future.result()
+        if not goal_handle.accepted:
+            self.get_logger().info('Goal rejected :(')
+            return
+        
+        self.get_logger().info('Goal accepted :)')
+
+        self._get_result_future = goal_handle.get_result_async()
+        self._get_result_future.add_done_callback(self.get_result_callback)
+
+    def get_result_callback(self, future):
+        result = future.result().result
+        self.get_logger().info('Result: {0}'.format(result.sequence))
+        rclpy.shutdown()
+
     
 def main (args=None):
     rclpy.init(args=args)
